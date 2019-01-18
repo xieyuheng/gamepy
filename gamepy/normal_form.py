d90c33ca7df908c1a17e1542579bcbf9a517d1fc
Xie Yuheng <xyheme@gmail.com>
2019-01-19 00:17:55 +0800

    up

--- gamepy/normal_form.py
+++ gamepy/normal_form.py
@@ -1,18 +1,65 @@
 import numpy as np
 import pandas as pd
 
+# player: str
+# action: str
+# strategy: (player, action)
+# strategies: pd.Series
+# opponent_strategies: pd.Series -- length == self.players.size - 1
+# strategy_profile: pd.Series -- length == self.players.size
+
 class c:
-    def __init__(self, players):
-        self.players = players
+    def __init__(self, players, payoff_tensor):
+        self.players = pd.Series(players, name="players")
+        self.player_shape = tuple(map(len, self.players.values))
+        self.payoff_tensor_shape = (*self.player_shape, len(players))
 
-        self.shape = tuple(map(
-            lambda x: len(x),
-            self.players.values()))
+        assert payoff_tensor.shape == self.payoff_tensor_shape
+        self.payoff_tensor = payoff_tensor
 
-        self.tensors = {}
-        self.tensor_size = np.array(self.shape).prod()
+    def Strategies(self, strategies, name="Strategies"):
+        return pd.Series(
+            strategies,
+            name=name,
+            index=self.players.index,
+        ).dropna()
 
-        for player in players:
-            self.tensors[player] = np.ndarray(
-                shape=self.shape,
-                buffer=np.zeros(self.tensor_size))
+    def strategy_index(self, strategy):
+        player, action = strategy
+        return self.players[player].index(action)
+
+    def payoff_array(self, strategy_profile):
+        strategy_profile = self.Strategies(strategy_profile)
+        assert strategy_profile.size == self.players.size
+        index_list = map(
+            lambda strategy: self.strategy_index(strategy),
+            strategy_profile.items())
+        return self.payoff_tensor[tuple(index_list)]
+
+    def payoff_series(self, strategy_profile):
+        return pd.Series(
+            self.payoff_array(strategy_profile),
+            name="playoffs",
+            index=self.players.index)
+
+    # def bast_response_p(self, strategy, opponent_strategies):
+    #     return
+
+    def responses(self, opponent_strategies):
+        opponent_strategies = self.Strategies(opponent_strategies)
+        assert opponent_strategies.size == self.players.size - 1
+        player = set(self.players.index) - set(opponent_strategies.index)
+        action_list = self.players[player]
+        print ("player = {}".format(player))
+
+    def bast_response(self, opponent_strategies):
+        return
+
+    # def opponent_strategies(self, strategy):
+    #     return
+
+    # def nash_equilibrium(self):
+    #     return
+
+    # def dominant_strategy(self):
+    #     return
--- tests/test_normal_form.py
+++ tests/test_normal_form.py
@@ -1,14 +1,79 @@
+import numpy as np
+import pandas as pd
+
 from gamepy import normal_form
 
-actions = [
-    "stays_silent",
-    "betrays",
-]
+def prisoner_s_dilemma():
+    action_list = [
+        "silent",
+        "betray",
+    ]
 
-players = {
-    "Alice": actions,
-    "Bob": actions,
-}
+    players = {
+        "Alice": action_list,
+        "Bob": action_list,
+    }
 
-def test_c():
-    game = normal_form.c (players)
+    payoff_tensor = np.array([
+        [[-1, -1], [-3, -0]],
+        [[-0, -3], [-2, -2]],
+    ])
+
+    return normal_form.c(players, payoff_tensor)
+
+def test_payoff_array():
+    game = prisoner_s_dilemma()
+
+    # strategy_profile: pd.Series like
+
+    np.testing.assert_array_equal(
+        game.payoff_array({
+            "Alice": "silent",
+            "Bob": "silent",
+        }),
+        np.array([-1, -1]))
+
+    np.testing.assert_array_equal(
+        game.payoff_array({
+            "Alice": "silent",
+            "Bob": "betray",
+        }),
+        np.array([-3, -0]))
+
+    np.testing.assert_array_equal(
+        game.payoff_array({
+            "Alice": "betray",
+            "Bob": "silent",
+        }),
+        np.array([-0, -3]))
+
+    np.testing.assert_array_equal(
+        game.payoff_array({
+            "Alice": "betray",
+            "Bob": "betray",
+        }),
+        np.array([-2, -2]))
+
+def test_payoff_series():
+    game = prisoner_s_dilemma()
+
+    payoff_series = game.payoff_series({
+        "Alice": "silent",
+        "Bob": "betray",
+    })
+    assert payoff_series["Alice"] == -3
+
+    # the order of dict does not matter
+
+    payoff_series = game.payoff_series({
+        "Bob": "betray",
+        "Alice": "silent",
+    })
+    assert payoff_series["Alice"] == -3
+
+def test_responses():
+    game = prisoner_s_dilemma()
+
+    game.responses({
+        "Bob": "betray",
+    })
