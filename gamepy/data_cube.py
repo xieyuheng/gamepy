ed26e7d6630b1a799fc0581facf2ef03ce17fee0
Xie Yuheng <xyheme@gmail.com>
2019-01-31 02:05:49 +0800

    - `.retract`

--- gamepy/data_cube.py
+++ gamepy/data_cube.py
@@ -4,6 +4,9 @@ import pandas as pd
 def list_intersect (x, y):
     return list (filter (lambda e: e in y, x))
 
+def list_difference (x, y):
+    return list (filter (lambda e: e not in y, x))
+
 class data_cube_c:
     def __init__ (self, df, attr_name_list, indi_name_list):
         column_name_list = list (df.columns)
@@ -19,8 +22,9 @@ class data_cube_c:
         s = "\n"
         s += self.df.__repr__ ()
         s += "\n"
-        s += "attr_name_list = {}\n" .format (self.attr_name_list)
-        s += "indi_name_list = {}\n" .format (self.indi_name_list)
+        s += "attr_name_list = {}" .format (self.attr_name_list)
+        s += "\n"
+        s += "indi_name_list = {}" .format (self.indi_name_list)
         return s
 
     def proj (self, itemdict):
@@ -40,4 +44,23 @@ class data_cube_c:
             self.indi_name_list)
 
     def retract (self, attr_name_list):
-        return
+        df = self.df
+        rest_name_list = list_difference (
+            self.attr_name_list, attr_name_list)
+        df = df.drop (columns = attr_name_list)
+        if hasattr (df.index, "name"):
+            index_name = df.index.name
+        else:
+            index_name = None
+        df.index = df.index.set_names (["_index"])
+        df = df.reset_index ()
+        df = df.groupby (
+            ["_index"] + rest_name_list,
+            as_index = False,
+        ) .sum ()
+        df = df.set_index ("_index")
+        df.index.name = index_name
+        return data_cube_c (
+            df,
+            self.attr_name_list,
+            self.indi_name_list)
--- tests/test_data_cube.py
+++ tests/test_data_cube.py
@@ -4,40 +4,58 @@ import pandas as pd
 import gamepy as gm
 
 abc_dic = {
-    "A": [1, 2, 3],
-    "B": [4, 5, 6],
-    "C": [7, 8, 9],
-    "I": [0, 1, 2],
-    "J": [2, 1, 0],
-    "X": [0, 0, 0], # this column will be ignored
+    "A": [1, 0, 2, 8, 3],
+    "B": [4, 4, 5, 5, 6],
+    "C": [7, 7, 8, 8, 9],
+    "I": [0, 1, 1, 1, 2],
+    "J": [2, 3, 1, 1, 0],
+    "X": [0, 0, 0, 0, 0], # this column will be ignored
 }
 
 abc_df = pd.DataFrame (
     abc_dic,
-    index = [100, 200, 300])
+    index = [1000, 1000, 2000, 2000, 3000])
 
 abc_cube = gm.data_cube_c (
     # "D" and "K" will be ignored
     abc_df, ["A", "B", "C", "D"], ["I", "J", "K"])
 
 def test_abc_cube ():
+    print ("<test_abc_cube>")
+    print ("abc_cube = {}" .format (abc_cube))
+    print ("</test_abc_cube>")
     assert abc_cube.attr_name_list == ["A", "B", "C"]
     assert abc_cube.indi_name_list == ["I", "J"]
-    pd.testing.assert_index_equal (
-        abc_cube.df.index,
-        pd.Index ([100, 200, 300]))
 
 def test_proj ():
     cube = abc_cube.proj ({
-        "A": 1,
         "B": 4,
+        "C": 7,
     })
+    print ("<test_proj>")
+    print ("cube = {}" .format (cube))
+    print ("</test_proj>")
+    assert cube.attr_name_list == ["A", "B", "C"]
+    assert cube.indi_name_list == ["I", "J"]
     assert type (cube) == gm.data_cube_c
 
-def test_proj ():
+def test_proj_drop ():
     cube = abc_cube.proj_drop ({
-        "A": 1,
         "B": 4,
+        "C": 7,
     })
-    assert cube.attr_name_list == ["C"]
+    print ("<test_proj_drop>")
+    print ("cube = {}" .format (cube))
+    print ("</test_proj_drop>")
+    assert cube.attr_name_list == ["A"]
+    assert cube.indi_name_list == ["I", "J"]
+    assert type (cube) == gm.data_cube_c
+
+def test_retract ():
+    cube = abc_cube.retract (["A"])
+    print ("<test_retract>")
+    print ("cube = {}" .format (cube))
+    print ("</test_retract>")
+    assert cube.attr_name_list == ["B", "C"]
+    assert cube.indi_name_list == ["I", "J"]
     assert type (cube) == gm.data_cube_c
